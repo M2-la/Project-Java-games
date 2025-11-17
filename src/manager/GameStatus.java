14fedba0f88b4dd692ccfbf94acffe1f1bebffb8
latif <M2-la@users.noreply.github.com>
2025-11-17 10:05:48 +0100

    feat[add manager game ]

---
+++ src/manager/GameStatus.java
@@ -0,0 +1,13 @@
+package manager;
+
+public enum GameStatus {
+    GAME_OVER,
+    PAUSE_RESUME,
+    RUNNING,
+    START_SCREEN,
+    MAP_SELECTION,
+    MISSSION_PASSED,
+    HELP_SCREEN,
+    ABOUT_SCREEN
+    
+}
